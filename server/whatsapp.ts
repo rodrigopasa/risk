8ae7c53e6ca4850cd6a14ac9834fa9b05ee405bb
Hisoka <rprodrigopasa@gmail.com>
2025-05-06 13:26:38 -0300

    Update whatsapp.ts

--- server/whatsapp.ts
+++ server/whatsapp.ts
@@ -63,9 +63,7 @@ export async function initializeWhatsApp(
       authStrategy: new LocalAuth({ dataPath: "./whatsapp-auth" }),
       puppeteer: {
         headless: true,
-        executablePath: process.env.NODE_ENV === 'production' 
-          ? undefined 
-          : '/nix/store/zi4f80l169xlmivz8vja8wlphq74qqk0-chromium-125.0.6422.141/bin/chromium',
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/nix/store/zi4f80l169xlmivz8vja8wlphq74qqk0-chromium-125.0.6422.141/bin/chromium',
         args: [
           '--no-sandbox',
           '--disable-setuid-sandbox',
